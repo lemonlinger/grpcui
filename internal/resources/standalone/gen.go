6a3a1935fa90965843e3fb0c0f6d8a77f3a8788f
limeng01 <limeng01@p1.com>
2019-09-04 20:53:38 +0800

    regenerate resource binary files via github.com/kevinburke/go-bindata

--- internal/resources/standalone/gen.go
+++ internal/resources/standalone/gen.go
@@ -1,12 +1,22 @@
 package standalone
 
-// The index template is done separate, so it's not in the TOC.
-// The go-bindata-all.sh script handles *.css, *.js, and *.png.
+// all binaries of files under /resources are combined into a single file 'bindata.go'.
 
-//go:generate go-bindata -out=index-template.go -pkg=standalone -func=GetIndexTemplate index-template.html
 //go:generate ./go-bindata-all.sh
 
 // GetResources is an exported accessor for resource file contents.
 func GetResources() map[string]func() []byte {
-	return go_bindata
+	m := map[string]func() []byte{}
+	names := AssetNames()
+	for _, name := range names {
+		m["/"+name] = func() []byte {
+			return MustAsset(name)
+		}
+	}
+	return m
+}
+
+// GetIndexTemplate is a dedicated exported accessor for index-template.html
+func GetIndexTemplate() []byte {
+	return MustAsset("index-template.html")
 }
--- internal/resources/standalone/go-bindata-all.sh
+++ internal/resources/standalone/go-bindata-all.sh
@@ -2,18 +2,11 @@
 
 set -e
 
+go get github.com/kevinburke/go-bindata
+
 # remove generated files first
-rm *.css.go *.png.go *.js.go *.svg.go 2>/dev/null || true
+rm bindata.go 2>/dev/null || true
 
 # then re-create them
-find . -name '*.css' -or -name '*.png' -or -name '*.js' -or -name '*.svg' \
-    | while read name; do
-        name="${name#./*}"
-
-        go_name="${name//\//_}"
-
-        func_name="${go_name//./_}"
-        func_name="${func_name//-/_}"
-
-        go-bindata -toc -out="${go_name}.go" -pkg=standalone -func "$func_name" "$name"
-      done
+find . -name '*.css' -or -name '*.png' -or -name '*.js' -or -name '*.svg' -or -name '*.html' | \
+    xargs go-bindata -pkg=standalone
